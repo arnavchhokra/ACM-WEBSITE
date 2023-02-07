448af5060a9aec9dfe34c5e8ca19e21020a836f0
Arnav Chhokra <arnavchhokra@gmail.com>
2023-02-07 20:38:42 +0530

    fixed post request

--- API/events/urls.py
+++ API/events/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('events/',views.events_list,name="events-list"),
-    path('events/<str:pk>/',views.events_detail, name="events-detail"),
+    path('events/get/<str:pk>/',views.events_detail, name="events-detail"),
     path('events/register/',views.events_post,name="events-register")
 ]
