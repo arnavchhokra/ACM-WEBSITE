c0e8389b42bef60518f4998f2ba1aab8640b183f
Arnav Chhokra <arnavchhokra@gmail.com>
2023-02-07 20:35:52 +0530

    Added url for post

--- API/events/urls.py
+++ API/events/urls.py
@@ -5,7 +5,5 @@ from . import views
 urlpatterns = [
     path('events/',views.events_list,name="events-list"),
     path('events/<str:pk>/',views.events_detail, name="events-detail"),
-  #  path('Bag/',views.orders_list),
-   # path('hello/', views.HelloView.as_view(), name ='hello'),
-   # path('Bag/<str:pk>/',views.orders_detail)
+    path('events/register/',views.events_post,name="events-register")
 ]
